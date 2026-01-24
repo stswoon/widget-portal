9297e7f804bfa1ef2eeed471449d246281c7ccdd
stswoon <xx@xx.xx>
2026-01-24 20:14:49 +0400

    upgrade 15->16
    fix revalidate via external DB-like

--- portal/src/app/api/revalidate/route.ts
+++ portal/src/app/api/revalidate/route.ts
@@ -1,6 +1,8 @@
 import { revalidatePath } from "next/cache";
 import { ROUTE_REGISTER_SERVICE } from "@/utils/RouteRegister.service";
 
+//export const runtime = "nodejs";
+
 export async function GET() {
   console.log("RevalidateRoute::routes=", ROUTE_REGISTER_SERVICE.getAll().join(", "));
   const routeList = ROUTE_REGISTER_SERVICE.getAll().join("\n");
@@ -8,6 +10,9 @@ export async function GET() {
     revalidatePath(path);
   });
   ROUTE_REGISTER_SERVICE.clear();
+
+  // revalidatePath("/portal");
+
   console.log("RevalidateRoute::finish");
   return new Response("OK" + "\n" + routeList);
 }
--- portal/src/utils/RouteRegister.service.ts
+++ portal/src/utils/RouteRegister.service.ts
@@ -1,25 +1,51 @@
 import { nanoid } from 'nanoid'
+import * as fs from "node:fs";
 
+const ROUTES_FILE_NAME = "./routes.txt"
+
+//singleton not works anymore (different runtime in routes and pages) so need to store data into external storage, e.g.
+//in DB or file
+//or revalidatePath("/portal");
+//or because config changed in cms so technically we know productId in advance so can revalidate full path to specific product.
+//near realize DB-like approach
 class RouteRegisterService {
   public readonly _ID = nanoid(); //singleton works
 
-  private routes = new Set<string>();
+  //private routes = new Set<string>();
+
+  #loadRoutes(): Set<string> {
+    if (!fs.existsSync(ROUTES_FILE_NAME)) {
+      this.#writeRoutes(new Set<string>());
+    }
+    const tmp = fs.readFileSync(ROUTES_FILE_NAME, 'utf-8');
+    const tmp2 = JSON.parse(tmp);
+    return new Set<string>(tmp2);
+  }
+
+  #writeRoutes(routes: Set<string>): void {
+    fs.writeFileSync(ROUTES_FILE_NAME, JSON.stringify([...routes], null, 2), 'utf-8');
+  }
 
   register(url: string): void {
     if (url.endsWith("/")) {
       url = url.slice(0, url.length - 1);
     }
     console.log(`PageRouteRegisterService[${this._ID}]::register url=${url}`);
-    this.routes.add(url);
+
+    const routes = this.#loadRoutes();
+    routes.add(url);
+    this.#writeRoutes(routes);
   }
 
   getAll(): string[] {
-    console.log(`PageRouteRegisterService[${this._ID}]::get all urls=${Array.from(this.routes).join(", ")}`);
-    return Array.from(this.routes);
+    const routes = this.#loadRoutes();
+
+    console.log(`PageRouteRegisterService[${this._ID}]::get all urls=${Array.from(routes).join(", ")}`);
+    return Array.from(routes);
   }
 
   clear(): void {
-    this.routes.clear();
+    this.#writeRoutes(new Set<string>());
   }
 }
 
